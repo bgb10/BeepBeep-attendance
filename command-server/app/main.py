17830f5d402b7fc671f2d2a79749fcd823d42003
bgb10 <bgb10@naver.com>
2024-03-10 17:27:59 +0900

    setting: try to connect activeMQ during startup

--- command-server/app/main.py
+++ command-server/app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from stomp import Connection
 
 app = FastAPI()
+
+
+def get_active_mq_connection():
+    # Implement connection logic here
+    connection = Connection([('localhost', 61613)])
+    connection.connect(wait=True)
+    return connection
+
+
+@app.on_event("startup")
+async def startup_event():
+    active_mq_connection = get_active_mq_connection()
+    print("Connected to ActiveMQ.")
 
 
 @app.get("/")
