74955d4c76f9572d408e284a27dc6ac9b846dcf9
bgb10 <bgb10@naver.com>
2024-03-12 17:37:34 +0900

    feat: implement client endpoint

--- command-server/app/command.py
+++ command-server/app/command.py
@@ -24,3 +24,23 @@ async def beacon_command(
     connection.send(body=json.dumps(message),
                     destination=f'/topic//command/beacon/{destination}')
     return "message sended."
+
+
+@router.get("/client")
+async def client(
+    destination: int,
+    sampleRate: int,
+    recordLength: int,
+    sleepTime: int,
+    connection: Connection = Depends(get_active_mq_connection)
+):
+    message = {
+        "statusCode": destination,
+        "samplerate": sampleRate,
+        "recordLength": recordLength,
+        "sleepTime": sleepTime
+    }
+
+    connection.send(body=json.dumps(message),
+                    destination=f'/topic//command/client/{destination}')
+    return "message sended."
