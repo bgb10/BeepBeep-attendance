930549a8c9581ef43938c303d9f7de9dfd2b5c7b
bgb10 <bgb10@naver.com>
2024-03-12 17:29:09 +0900

    feat: implement beacon endpoint

---
+++ command-server/app/command.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from mq_connection import get_active_mq_connection
+from stomp import Connection
+import json
+
+router = APIRouter()
+
+
+@router.get("/beacon")
+async def beacon_command(
+    destination: int,
+    sampleRate: int,
+    recordLength: int,
+    sleepTime: int,
+    connection: Connection = Depends(get_active_mq_connection)
+):
+    message = {
+        "statusCode": destination,
+        "samplerate": sampleRate,
+        "recordLength": recordLength,
+        "sleepTime": sleepTime
+    }
+
+    connection.send(body=json.dumps(message),
+                    destination=f'/topic//command/beacon/{destination}')
+    return "message sended."
--- command-server/app/main.py
+++ command-server/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Depends
 from stomp import Connection
 from mq_connection import get_active_mq_connection
+from command import router as command_router
 
 app = FastAPI()
 
+app.include_router(command_router, prefix="/commands")
 
 
 @app.on_event("startup")
